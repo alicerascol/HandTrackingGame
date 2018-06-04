62eb5811b2d606d04f5ee4410305fe2ef543b4ca
Alice Rascol <alice.rascol@mindit.io>
2018-06-04 17:32:06 +0300

    register&login scene

--- Assets/scripts/login&register/Login.cs
+++ Assets/scripts/login&register/Login.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 using System;
 using System.Text.RegularExpressions;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Login : MonoBehaviour {
 
 	public GameObject username;
 	public GameObject password;
-	public GameObject loginButton;
+	public Button loginButton;
 	private string Username;
 	private string Password;
+
+	public Text retryText;
+	public GameObject retryPanel;
+	private float restartDelay = 2f;
+	private bool hasLoggedIn = false;
+
+	private string CheckIfExistsURL = "http://localhost/unity_game/check_for_login.php";
+
 
 	// Use this for initialization
 	void Start () {
@@ -31,5 +40,49 @@ public class Login : MonoBehaviour {
 		//in unity variabilele publice trebuie asignate
 		Username = username.GetComponent<InputField> ().text;
 		Password = password.GetComponent<InputField> ().text;
+		if(Username != "" && Password.Length == 6 && !hasLoggedIn) {
+			hasLoggedIn = true; 
+			WWWForm form = new WWWForm();
+			//making the form
+			form.AddField("usernamePost", Username);
+			form.AddField("passwordPost", Password);
+			Debug.Log(Username + " " + Password + " " + form);
+			WWW itemsData = new WWW(CheckIfExistsURL, form);
+			loginButton.onClick.AddListener(delegate {CheckIfExists(itemsData, Username, Password);});
+		}
+	}
+
+	private string response;
+    //Our Coroutine for getting the data
+	IEnumerator WaitForRequest(WWW www) {
+	    yield return www;
+	     
+	    //  // check for errors
+	    // if (www.error == null) {
+	    //     //Assign the data that was fetched to the variable answer
+		   //  response = www.text.ToString();
+	    //     Debug.Log(response);
+	    // } else {
+	    //     Debug.Log("WWW Error: "+ www.error);
+	    // }    
+ 	}
+ 
+
+	public void CheckIfExists(WWW itemsData, string username, string password) {
+		//Start the Coroutine
+        StartCoroutine(WaitForRequest(itemsData));
+        if (itemsData.text.ToString().Equals("User has logged in")) {
+        	//treci la scena urmatoare
+        } else {
+        	retryPanel.SetActive(true);
+        	retryText.text = itemsData.text.ToString();
+        	Invoke("RestartScene", restartDelay);
+        }
+	}
+
+	public void RestartScene() {
+		//current scene -> 
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().name)
+		SceneManager.LoadScene("RegisterAndLogin");
 	}
 }
--- Assets/scripts/login&register/Register.cs
+++ Assets/scripts/login&register/Register.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System;
 using System.Text.RegularExpressions;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Register : MonoBehaviour {
 
@@ -11,17 +12,20 @@ public class Register : MonoBehaviour {
 	public GameObject email;
 	public GameObject password;
 	public GameObject confirmPassword;
-	public GameObject registerButton;
+	public Button registerButton;
 	private string Username;
 	private string Email;
 	private string Password;
 	private string ConfirmPassword;
 
+	private string CreateUserURL = "http://localhost/unity_game/insert_new_user.php";
+	private string CheckIfExistsURL = "http://localhost/unity_game/check_if_user_exists.php";
 
-	// Use this for initialization
-	void Start () {
+	private bool userIsCreated = false;
 
-	}
+	public Text retryText;
+	public GameObject retryPanel;
+	private float restartDelay = 2f;
 
 	// Update is called once per frame
 	void Update () {
@@ -37,11 +41,72 @@ public class Register : MonoBehaviour {
 			if (confirmPassword.GetComponent<InputField> ().isFocused)
 				registerButton.GetComponent<Button> ().Select();
 		}
+
 		//in unity variabilele publice trebuie asignate
 		Username = username.GetComponent<InputField> ().text;
 		Email = email.GetComponent<InputField> ().text;
 		Password = password.GetComponent<InputField> ().text;
 		ConfirmPassword = confirmPassword.GetComponent<InputField> ().text;
+		
+		if(Username != "" && Password != "" && Email != "" && ConfirmPassword != "" && !userIsCreated) {
+			userIsCreated = true;
 
+	        WWWForm form = new WWWForm();
+			//making the form
+			form.AddField("usernamePost", Username);
+			form.AddField("emailPost", Email);
+			WWW itemsData = new WWW(CheckIfExistsURL, form);
+			registerButton.onClick.AddListener(delegate {CheckIfExists(itemsData, Username, Password, Email);});
+
+		}
+	}
+
+	private string response;
+    //Our Coroutine for getting the data
+	IEnumerator WaitForRequest(WWW www) {
+	    yield return www;
+	     
+	    //  // check for errors
+	    // if (www.error == null) {
+	    //     //Assign the data that was fetched to the variable answer
+		   //  response = www.text.ToString();
+	    //     Debug.Log(response);
+	    // } else {
+	    //     Debug.Log("WWW Error: "+ www.error);
+	    // }    
+ 	}
+ 
+
+	public void CheckIfExists(WWW itemsData, string username, string password, string email) {
+		//Start the Coroutine
+        StartCoroutine(WaitForRequest(itemsData));
+        if (itemsData.text.ToString().Equals("Everything ok.")) {
+        	CreateUser(username, password, email);
+        	//treci la scena urmatoare
+        } else {
+        	retryPanel.SetActive(true);
+        	retryText.text = itemsData.text.ToString();
+        	Invoke("RestartScene", restartDelay);
+
+        }
+	}
+
+	public void RestartScene() {
+		//current scene -> 
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().name)
+		SceneManager.LoadScene("RegisterAndLogin");
+	}
+
+	public void CreateUser(string username, string password, string email){
+		//class that allows us to send a form to a php; trigger our php from unuity
+		WWWForm form = new WWWForm();
+		//making the form
+		form.AddField("usernamePost", username);
+		form.AddField("passwordPost", password);
+		form.AddField("emailPost", email);
+		//simple access to web pages
+		//connect with the url and send the form to the php file
+		WWW www = new WWW(CreateUserURL, form);
+		Debug.Log("Registration completed");
 	}
 }
